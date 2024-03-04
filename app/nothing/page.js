686312e2db3c674536853c581861807c5a4cec04
LalitM <102734383+lalitm1004@users.noreply.github.com>
2024-03-04 16:54:43 +0530

    Update page.js
    Update kishika's word

--- app/nothing/page.js
+++ app/nothing/page.js
@@ -60,7 +60,7 @@ export default function Nothing() {
           "yoasobi", // Pustak
           "a crystal", // Shashwat
           "useless", // Rachit
-          "supercalifragilisticexpialidocious" // Kishika
+          "me.", // Kishika
         ]
         const textToStreamIntro = `hello, my name is lalit\nand i am `
         const textToStreamOutro = `\n\nwelcome to the l in skarj.pl`
