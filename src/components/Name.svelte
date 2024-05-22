4a06293a06bb8c33c6c881d17e66f2ed7c249286
Lalit M <lalitm1004@gmail.com>
2024-05-22 23:32:12 +0530

    media queries

--- src/components/Name.svelte
+++ src/components/Name.svelte
@@ -3,7 +3,7 @@
         <img
             src={`/alphabet/${letter}.svg`}
             alt={letter}
-            class={`hover-animation`}
+            class={`hover-animation ${letter === "i" ? "i": ""}`}
             style={`transform: translateY(-10px); animation-delay: ${(index + 1) * 200}ms`}
         />
     {/each}
@@ -11,8 +11,25 @@
 
 <style>
     img {
-        height: 85px;
-        width: 85px;
+        height: 84px;
+        width: 84px;
+    }
+
+    .i {
+        height: 84px;
+        width: 56px;
+    }
+
+    @media screen and (min-width: 600px) {
+        img {
+            height: 96px;
+            width: 96px;
+        }
+
+        .i {
+            height: 96px;
+            width: 64px;
+        }   
     }
 
     @keyframes hover {
